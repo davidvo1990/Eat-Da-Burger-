a39c982f6e78b5c8468865892a2bf85aea128c5b
David <davidvo1990@gmail.com>
2019-04-30 02:19:32 -0500

    dv

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Jr. Sandwich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('French Fries');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
